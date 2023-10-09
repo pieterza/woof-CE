e5999b88f282618c8bc895dfed2d645164108132
Pieter Steyn <pieterste@gmail.com>
2023-10-09 20:14:26 +0200

    Fix jwm configs.

--- woof-code/rootfs-skeleton/etc/init.d/90ccs.sh
+++ woof-code/rootfs-skeleton/etc/init.d/90ccs.sh
@@ -3,6 +3,10 @@
 echo "Setting up SIP profile"
 mkdir -p /root/.config/jami
 cp /etc/ccs/dring.yml  /root/.config/jami/
+echo "Overwriting jwm"
+rm -rf /root/.jwm
+cp -r /etc/ccs/.jwm /root
+cp /etc/ccs/xdg/templates/_root_.jwmrc /root/.jwmrc
 echo "Overwriting jwm tray"
 cp /etc/ccs/jwm* /root/.jwm/
 #echo "Setting wallpaper"
