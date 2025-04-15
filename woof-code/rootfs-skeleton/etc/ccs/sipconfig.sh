62cf4ca89b4e781cf790acbce31491a25bde84cf
Pieter Steyn <pieterste@gmail.com>
2025-04-15 13:50:30 +0200

    Set the Sip username/extention to final digit of IP + 6000

--- woof-code/rootfs-skeleton/etc/ccs/sipconfig.sh
+++ woof-code/rootfs-skeleton/etc/ccs/sipconfig.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
 echo 1 > /tmp/sipconf
+IP=$(ifconfig | grep 192.168 | sed 's/.*inet addr:\(.*\) Bcast.*/\1/g')
+FINAL_DIGIT=$(echo $IP | awk -F . {'print $NF'})
+EXTENTION=$(expr $FINAL_DIGIT + 6000)
+echo $FINAL_DIGIT
+echo $EXTENTION
+
+# TODO:
+
+# Find ip for this mac, if not exists, store it so its locked.
+# mysql/php thing for that.
+# for now, set the extention to the IP + 6000 as per instruction
+
+echo "Changing extention to $EXTENTION"
+sed -i "s/username: .*/username: $EXTENTION/g" ~/.config/jami/dring.yml
