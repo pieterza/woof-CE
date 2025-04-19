3fee632c695581f9a5c3975a19962cd87e9628ae
Pieter Steyn <pieterste@gmail.com>
2025-04-19 18:24:17 +0200

    Debug and wait sipconfig.sh

--- woof-code/rootfs-skeleton/etc/ccs/sipconfig.sh
+++ woof-code/rootfs-skeleton/etc/ccs/sipconfig.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
-echo 1 > /tmp/sipconf
+echo "started from $0" >> /tmp/sipconf
+while ! ifconfig | grep 192.168 > /dev/null; do
+  echo "Waiting for IP to show: $(ifconfig)" >> /tmp/sipconf
+  sleep 1
+done
 IP=$(ifconfig | grep 192.168 | sed 's/.*inet addr:\(.*\) Bcast.*/\1/g')
 FINAL_DIGIT=$(echo $IP | awk -F . {'print $NF'})
 EXTENTION=$(expr $FINAL_DIGIT + 6000)
 echo $FINAL_DIGIT
 echo $EXTENTION
 
+echo $FINAL_DIGIT >> /tmp/sipconf
+echo $EXTENTION >> /tmp/sipconf
 # TODO:
-
-# Find ip for this mac, if not exists, store it so its locked.
-# mysql/php thing for that.
-# for now, set the extention to the IP + 6000 as per instruction
 
 echo "Changing extention to $EXTENTION"
 sed -i "s/username: .*/username: $EXTENTION/g" ~/.config/jami/dring.yml
