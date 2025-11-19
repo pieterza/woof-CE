71f232c7c7f5834f234d715a23419d4ac8eeb338
Pieter Steyn <pieters@localhost.localdomain>
2025-11-19 19:04:43 +0200

    Ensure to also capture the input device.

--- woof-code/rootfs-skeleton/etc/ccs/headsetd.sh
+++ woof-code/rootfs-skeleton/etc/ccs/headsetd.sh
@@ -5,17 +5,22 @@
 
 LINPHONE_RC="/home/spot/.config/linphone/linphonerc"
 
+
 detect_usb_audio() {
-    wpctl status | grep USB | grep vol | grep -oP '\d+\.\s*\K[^[]*' | head -n1 | xargs
+    wpctl status | grep USB | grep vol | grep -i stereo | grep -oP '\d+\.\s*\K[^[]*' | head -n1 | xargs
+}
+
+detect_usb_audio_mic() {
+    wpctl status | grep USB | grep -vol | grep -i mono  | grep -oP '\d+\.\s*\K[^[]*' | head -n1 | xargs
 }
 
 USB_DEV="PulseAudio Unknown: $(detect_usb_audio)"
-
+USB_DEV_MIC="PulseAudio Unknown $(detect_usb_audio_mic)"
 sed -i "
   s|^playback_dev_id=.*|playback_dev_id=${USB_DEV}|
   s|^ringer_dev_id=.*|ringer_dev_id=${USB_DEV}|
-  s|^capture_dev_id=.*|capture_dev_id=${USB_DEV}|
-  s|^media_dev_id=.*|media_dev_id=${USB_DEV}|
+  s|^capture_dev_id=.*|capture_dev_id=${USB_DEV_MIC}|
+  s|^media_dev_id=.*|media_dev_id=${USB_MEDIA}|
 "  $LINPHONE_RC
 
-echo "[INFO] Switched Linphone sound devices to: $USB_DEV"
+echo "[INFO] Switched Linphone sound devices to: $USB_DEV for output and $USB_DEV_MIC for input"
