aed9f91c948dcf955935a16cef705296200acc8b
Pieter Steyn <pieterste@gmail.com>
2026-05-21 13:43:11 +0200

    Try fix no proxy.

--- woof-code/rootfs-skeleton/etc/init.d/90ccs.sh
+++ woof-code/rootfs-skeleton/etc/init.d/90ccs.sh
@@ -21,5 +21,11 @@ mkdir -p /etc/firefox/policies
 cp /etc/ccs/firefox/policies/* /etc/firefox/policies
 cp /etc/ccs/homefirefox/firefox/j1pg8r1a.default-esr/prefs.js /home/spot/.mozilla/firefox/j1pg8r1a.default-esr/prefs.js
 cp /etc/ccs/homefirefox/firefox/j1pg8r1a.default-esr/logins* /home/spot/.mozilla/firefox/j1pg8r1a.default-esr/
-chown spot:spot /home/spot/.mozilla/firefox/j1pg8r1a.default-esr/*
+
+echo 'export no_proxy="localhost,127.0.0.1,192.168.0.0/16,.customercaresolutions.local,ag,ag.customercaresolutions.local,dc,dco"' >> /home/spot/.bashrc 
+echo 'export NO_PROXY=$no_proxy' >> /home/spot/.bashrc
+echo 'export no_proxy="localhost,127.0.0.1,192.168.0.0/16,.customercaresolutions.local,ag,ag.customercaresolutions.local,dc,dco"' >> /home/spot/.bash_profile
+echo 'export NO_PROXY=$no_proxy' >> /home/spot/.bash_profile
+chown -R spot:spot /home/spot/.mozilla/
+chown -R spot:spot /home/spot/.bash*
 set_bg /etc/ccs/wall.svg
--- woof-code/rootfs-skeleton/home/spot/.bashrc
+++ woof-code/rootfs-skeleton/home/spot/.bashrc
@@ -10,5 +10,5 @@ if [ -x /usr/lib/command-not-found ]; then
  }
 fi
 
-export no_proxy="192.168.*,.customercaresolutions.local,ag,dc,dco"
+export no_proxy="localhost,127.0.0.1,192.168.0.0/16,.customercaresolutions.local,ag,ag.customercaresolutions.local,dc,dco"
 export NO_PROXY="$no_proxy"
